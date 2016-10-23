5b18b5fcc96aafd960e3d5b43a649cf6b49d624b
Christopher <bengtsson.christopher@hotmail.com>
2016-10-23 23:33:25 +0200

    push hole project

---
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/LoginActivity.java
@@ -0,0 +1,225 @@
+package com.github.nkzawa.socketio.androidchat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+
+/**
+ * A login screen that offers login via username.
+ */
+public class LoginActivity extends Activity {
+
+    private EditText mUsernameView;
+
+    private String mUsername;
+
+    private Socket mSocket;
+
+    /*private static TextView mLocation;
+
+    private BroadcastReceiver broadcastReceiver;*/
+    static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ChatApplication app = (ChatApplication) getApplication();
+        mSocket = app.getSocket();
+
+        // Set up the login form.
+        mUsernameView = (EditText) findViewById(R.id.username_input);
+        mUsernameView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mSocket.on("login", onLogin);
+
+        /*mLocation = (TextView) findViewById(R.id.textView);*/
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        } else {
+            /*Intent intent = new Intent(LoginActivity.this, BackgroundLocationService.class);
+            startService(intent);
+
+            printLocation();*/
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();/*
+        //mLocation.setText("onResume");
+        if(!Utils.isMyServiceRunning(this, BackgroundLocationService.class)) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+                *//*printLocation();*//*
+            }
+
+            else {
+                *//*Intent intent = new Intent(LoginActivity.this, BackgroundLocationService.class);
+                startService(intent);
+
+                printLocation();*//*
+            }
+        }
+
+    */}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        /*this.unregisterReceiver(this.broadcastReceiver);
+        Log.i(TAG,"Paused Location Updates");
+        Intent intent = new Intent(LoginActivity.this, BackgroundLocationService.class);
+        stopService(intent);*/
+        //mLocation.append("onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        /*Intent intent = new Intent(LoginActivity.this, BackgroundLocationService.class);
+        stopService(intent);*/
+
+        mSocket.off("login", onLogin);
+    }
+
+    /**
+     * Attempts to sign in the account specified by the login form.
+     * If there are form errors (invalid username, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        // Reset errors.
+        mUsernameView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString().trim();
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            mUsernameView.setError(getString(R.string.error_field_required));
+            mUsernameView.requestFocus();
+            return;
+        }
+
+        mUsername = username;
+
+        // perform the user login attempt.
+        mSocket.emit("add user", username);
+    }
+
+    private Emitter.Listener onLogin = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONObject data = (JSONObject) args[0];
+
+            int numUsers;
+            try {
+                numUsers = data.getInt("numUsers");
+            } catch (JSONException e) {
+                return;
+            }
+
+            Intent intent = new Intent();
+            intent.putExtra("username", mUsername);
+            intent.putExtra("numUsers", numUsers);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    };
+
+    //------------------------------------------->GPS codes<---------------------------------------------------------------------------------------------
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent intent = new Intent(LoginActivity.this, BackgroundLocationService.class);
+                    startService(intent);
+
+                } else {
+
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+    /*public void printLocation() {
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    mLocation.setText("\n" +intent.getExtras().get("coordinates"));
+
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }*/
+}
+
+
